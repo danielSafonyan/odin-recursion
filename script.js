e660a34fa22eee0f7e046eae96536d6f09296d32
danielSafonyan <danielsafonyan@gmail.com>
2022-09-27 17:10:08 +0100

    Solved Fibonacci the iterative way

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hello, World!');
+function fibIter(num) {
+    const arr = [0, 1]
+    for (let i = 0; i < num - 2; i++) {
+        arr.push(arr[i] + arr[i+1])
+    }
+    return arr;
+}
+
+console.log(fibIter(8));
