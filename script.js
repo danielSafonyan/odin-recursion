5b924e989623ceb03fcd4b3c130e0f9ab2ab8ab3
danielSafonyan <danielsafonyan@gmail.com>
2022-09-27 17:46:04 +0100

    Finished Fibonacci seq recursively

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function fibIter(num) {
     const arr = [0, 1]
-    for (let i = 0; i < num - 2; i++) {
+    for (let i = 0; i < num - 1; i++) {
         arr.push(arr[i] + arr[i+1])
     }
     return arr;
 }
 
-console.log(fibIter(8));
+function fibRec(num) {
+    if (num < 2) return num;
+    else return fibRec(num - 1) + fibRec(num - 2)
+}
